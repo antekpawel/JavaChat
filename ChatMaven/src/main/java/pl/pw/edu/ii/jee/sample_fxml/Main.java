7429a8bb171b18faa262392a9e688c60ca05ac26
Antek <Antek@DESKTOP-FN6MRBH.home>
2022-02-04 16:43:19 +0100

    Add maven

---
+++ ChatMaven/src/main/java/pl/pw/edu/ii/jee/sample_fxml/Main.java
@@ -0,0 +1,59 @@
+package pl.pw.edu.ii.jee.sample_fxml;
+	
+import java.io.IOException;
+import java.util.Optional;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	
+	public static final String appName = "Sockets-JavaFX-MVC";
+
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			ViewLoader<AnchorPane, ChatController> viewLoader = new ViewLoader<>("Sample.fxml");
+			viewLoader.getController().setUserName(getUserName());
+			viewLoader.getController().setHost("localhost");
+			viewLoader.getController().setPort(9001);
+			viewLoader.getController().run();
+			Scene scene = new Scene(viewLoader.getLayout());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle(appName);
+			primaryStage.setOnHiding( e -> primaryStage_Hiding(e, viewLoader.getController()));
+			primaryStage.show();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void primaryStage_Hiding(WindowEvent e, ChatController controller) {
+		try {
+			controller.close();
+		} catch (IOException e1) {
+			System.out.print("Error primaryStage_Hiding");
+		}
+	}
+
+	private String getUserName() {
+		TextInputDialog textInputDialog = new TextInputDialog("Anonymous");
+		textInputDialog.setTitle("Add user");
+		textInputDialog.setHeaderText("Welcome!");
+		textInputDialog.setContentText("Set your username:");
+		Optional<String> result = textInputDialog.showAndWait();
+		return result.orElse("Anonymous");
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
